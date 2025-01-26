bc7666569a97dea9b98d65c843e831f3ae5985f5
JDBlythe <jdblythe1@buffs.wtamu.edu>
2025-01-25 21:19:53 -0600

    Add files via upload

---
+++ HomeWork1.cs
@@ -0,0 +1,12 @@
+﻿namespace HomeWork1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float x = 2.5f;
+        float y = 3.3f;
+        float z = 4*x*x + 3*y;
+        Console.Write("X= {0}, Y= {1}\nThe value of Z is: {2}", x, y, z);
+    }
+}
